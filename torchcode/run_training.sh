486917c0d04301865a9b89da9ac775a26bb6b324
jonasw247 <jonas.weidner@outlook.de>
2023-03-06 16:43:30 +0100

    parameters...

--- torchcode/run_training.sh
+++ torchcode/run_training.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 main.py --isnewsave --outputmode 3 --gpu 0 --purpose necr-normpet-nonorm-batch32-wd05-xyz --batch_size 32 --num_workers 5 --starttrain 0 --endtrain 80000 --startval 80000 --endval 88000 --dropoutrate 0.0 --lr 0.00006 --lr_scheduler_rate 0.999997 --weight_decay_sgd 0.05 --savelogdir="./result/"
+python3 main.py --isnewsave --outputmode 3 --gpu 5 --purpose jonasTest_with10kSamplesDiffBGTissueCompareNowWM --batch_size 32 --num_workers 5 --starttrain 0 --endtrain 8000 --startval 8000 --endval 9984 --dropoutrate 0.0 --lr 0.00006 --lr_scheduler_rate 0.999997 --weight_decay_sgd 0.05 --savelogdir="./result/" --num_epochs 50
